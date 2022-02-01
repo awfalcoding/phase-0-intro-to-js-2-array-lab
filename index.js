001175320d15159acfbc2f5314e4771c83ac9ace
Aaron Falzone <aw.falzone@gmail.com>
2022-02-01 09:34:00 -0700

    commit tree 373b3d4c6233eba06ca1b1518aa37593003cb16c

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+    }
+
+    function destructivelyPrependCat(name) {
+       return cats.unshift(name);
+    }
+
+    function destructivelyRemoveLastCat() {
+        cats.pop();
+    }
+
+    function destructivelyRemoveFirstCat() {
+        cats.shift();
+    }
+    function appendCat(name) {
+        return [...cats, name];
+    }
+
+    function prependCat(name) {
+        return [name, ...cats];
+    }
+
+    function removeFirstCat() {
+        cats.shift();
+    }
+
+    function destructivelyRemoveLastCat() {
+        cats.shift();
+    }
+
+    function removeLastCat() {
+        return cats.slice(1)
+    }
